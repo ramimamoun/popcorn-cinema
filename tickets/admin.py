b23eb2f12b3bbef5c4225a2cd86322c2d4ab91c7
rami <rramimamoun94@gmail.com>
2021-08-12 00:29:55 +0300

    2 git

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie
+
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+
+    list_display = ['movie','hall','date','price','is_published']
+
+admin.site.register(Movie,MovieAdmin)
--- tickets/models.py
+++ tickets/models.py
@@ -1,8 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from datetime import datetime
+# from django.utils.text import slugify
+from django.template import defaultfilters
+from unidecode import unidecode
+
+
 
 # Create your models here.
 class Movie(models.Model):
-    hall = models.CharField(max_length=10)
     movie = models.CharField(max_length=10)
+    hall = models.CharField(max_length=10)
     date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    Vacancy = models.IntegerField(default=1)
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(blank=True,null=True, allow_unicode=True)
 
+    def __unicode__(self):
+        return self.movie
+
+
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.movie)
+        self.slug = defaultfilters.slugify(unidecode(self.movie))
+        super(Movie,self).save(*args,**kwargs)
+
+    def __str__(self):
+        return self.movie
